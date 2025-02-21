f83df3aadae3d1fe768a90d8a97e85db766912bf
András Bidlo <bidloandras@gmail.com>
2025-02-21 18:24:58 +0100

    Add frontend with drag&drop

--- frontend/src/taskboard/components/task-table/task-table.component.ts
+++ frontend/src/taskboard/components/task-table/task-table.component.ts
@@ -1,34 +1,39 @@
-import {Component, OnInit} from '@angular/core';
-import {
-  MatCell,
-  MatCellDef,
-  MatColumnDef,
-  MatHeaderCell, MatHeaderCellDef,
-  MatHeaderRow,
-  MatHeaderRowDef,
-  MatRow, MatRowDef, MatTable, MatTableDataSource
-} from "@angular/material/table";
+import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDropList, CdkDrag } from '@angular/cdk/drag-drop';
+import { MatTableDataSource } from '@angular/material/table';
 import {TaskDto} from '../../client/model/taskDto';
 import {TasksService} from '../../services/tasks.service';
 
+/**
+ * @title Drag&Drop connected sorting
+ * Source of this component: https://material.angular.io/cdk/drag-drop/overview#transferring-items-between-lists
+ * And the original task-table
+ */
 @Component({
   selector: 'taskboard-task-table',
-  imports: [
-    MatCell,
-    MatCellDef,
-    MatColumnDef,
-    MatHeaderCell,
-    MatHeaderRow,
-    MatHeaderRowDef,
-    MatRow,
-    MatRowDef,
-    MatTable,
-    MatHeaderCellDef
-  ],
-  templateUrl: './task-table.component.html',
-  styleUrl: './task-table.component.css'
+  templateUrl: 'task-table.component.html',
+  styleUrl: 'task-table.component.css',
+  imports: [CdkDropList, CdkDrag],
 })
-export class TaskTableComponent implements OnInit {
+export class TaskTableComponent  implements OnInit {
+  todo: TaskDto[] = [];
+  done: TaskDto[] = [];
+  doing: TaskDto[] = [];
+
+  drop(event: CdkDragDrop<TaskDto[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+
+  }
+
   displayedColumns: string[] = ['title', 'description', 'status', 'assignee'];
   dataSource = new MatTableDataSource<TaskDto>([]);
 
@@ -39,6 +44,10 @@ export class TaskTableComponent implements OnInit {
   ngOnInit() {
     this.tasksService.getTasks().then(tasks => {
       this.dataSource.data = tasks;
+      this.todo = tasks.filter(task => task.status === 'TODO');
+      this.done = tasks.filter(task => task.status === 'DONE');
+      this.doing = tasks.filter(task => task.status === 'DOING');
     });
+
   }
 }
